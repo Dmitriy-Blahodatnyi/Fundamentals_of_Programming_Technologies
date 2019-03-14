31544f4f388ed8c6d17227454d1af74606261780
Dmitriy-Blahodatnyi <dbrlmola82@gmail.com>
2019-03-14 20:59:48 +0200

    refactor

--- matrix/src/main/java/com/project/matrix/Matrix.java
+++ matrix/src/main/java/com/project/matrix/Matrix.java
@@ -151,10 +151,11 @@ public class Matrix {
 
         return IntStream
                 .range(0, array[0].length)
-                .mapToDouble(i ->
-                        pow(-1, i)
+                .mapToDouble(
+                        i -> pow(-1, i)
                                 * array[0][i]
-                                * findDeterminant(findMinor(new Matrix(array), 0, i)))
+                                * findDeterminant(findMinor(new Matrix(array), 0, i))
+                )
                 .sum();
     }
 

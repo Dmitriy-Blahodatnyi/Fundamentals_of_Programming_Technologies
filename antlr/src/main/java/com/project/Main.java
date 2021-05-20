362f22c70f83a69a300989405ab16e7da94604c1
Dmytro Blahodatny <dbrlmola82@gmail.com>
2021-05-20 19:59:51 +0300

    Making pulic running class

--- antlr/src/main/java/com/project/Main.java
+++ antlr/src/main/java/com/project/Main.java
@@ -1,6 +1,8 @@
+package com.project;
+
 import com.project.evaluator.Evaluator;
 
-class Main {
+public class Main {
     public static void main(String... args) {
         new Evaluator().evaluate("B=[[41.23]]");
     }

24453e3f439ffdf16a193f6d9d68419b2d10782d
Dmytro Blahodatny <dbrlmola82@gmail.com>
2021-05-20 19:06:00 +0300

    Fixed Travis

--- antlr/src/main/java/Main.java
+++ antlr/src/main/java/Main.java
@@ -1,7 +1,7 @@
 import com.project.evaluator.Evaluator;
 
 class Main {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         new Evaluator().evaluate("B=[[41.23]]");
     }
 }
